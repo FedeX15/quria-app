43116d1b1842e3606196864d91e1e82901935447
Federico Matteoni <fedex15@live.it>
2018-05-07 12:47:45 +0200

    Video EL1

--- app/src/main/java/com/fexed/quriacompanion/CampagnaFragment.java
+++ app/src/main/java/com/fexed/quriacompanion/CampagnaFragment.java
@@ -1,10 +1,13 @@
 package com.fexed.quriacompanion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CampagnaFragment extends Fragment {
     public CampagnaFragment() {
@@ -20,7 +23,18 @@ public class CampagnaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.campagnafragment, container, false);
+
+        View view = inflater.inflate(R.layout.campagnafragment, container, false);
+
+        Button el1 = (Button) view.findViewById(R.id.el1_btn);
+        el1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://youtu.be/NVbkZG-ZICI")));
+            }
+        });
+
+        return view;
     }
 
 }
